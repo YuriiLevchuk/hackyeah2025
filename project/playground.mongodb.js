8f7f30b577cfad6d211864964dcb5a77bf31bf19
Vitaliy-Wyzer <clayrenno@gmail.com>
2025-10-04 17:12:12 +0200

    added user model

--- project/playground.mongodb.js
+++ project/playground.mongodb.js
@@ -1,29 +1,47 @@
 // Switch to (or create) a database
 use('TransportDelayDB');
 
-db.User.InsertOne({
-  user_id: "u001",
-  first_name: "John",
-  last_name: "Doe",
-  email: "john.doe@example.com",
-  password_hash: "$2b$10$...",
+// db.User.insertOne({
+//   user_id: "u001",  
+//   first_name: "John",
+//   last_name: "Doe",
+//   email: "john.doe@example.com",
+//   password_hash: "$2b$10$...",
+//   created_at: new Date(),
+//   updated_at: new Date()
+// })
+
+// db.Station.insertOne({
+//   station_id: "s001",
+//   station_name: "TestStation",
+//   city: "Wasrzawa",
+//   address: "Ul. testowa 1",
+//   latitude: 40.7128,
+//   longitude: -74.0060,
+//   transport_type: "Train"
+// })
+
+// db.Transport.insertOne({
+//   transport_id: "t001",
+//   transport_type: "Train",
+//   stop_list: ["Station A", "Station B", "Station C"]
+// })
+
+db.Event.insertOne({
+  event_id: "e001", // optional human-readable ID
+  type: "train_failure", // e.g., "traffic_jam", "accident", "delay"
+  reported_at: new Date(), // when first reported
+  latitude: 21.0122,
+  longtitude: 52.2297,
+  severity: "high", // "low", "medium", "high"
+  confirmed_count: 1,
+  status: "new", // "new", "confirmed", "resolved"
+  resolved_at: null, // set when resolved
+  duration_minutes: null, // calculated later
+  description: "Awaría taboru na linii SKM S1",
+  affected_routes: ["S1"], // array of route IDs
+  source: "system", // "user", "operator", "system"
   created_at: new Date(),
   updated_at: new Date()
-})
-
-db.Station.InsertOne({
-  station_id: "s001",
-  station_name: "TestStation",
-  city: "Wasrzawa",
-  address: "Ul. testowa 1",
-  latitude: 40.7128,
-  longitude: -74.0060,
-  transport_type: "Train"
-})
-
-db.Transport.InsertOne({
-  transport_id: "t001",
-  transport_type: "Train",
-  stop_list: ["Station A", "Station B", "Station C"]
-})
+});
 
